744b4e1653c8e4bcc50a8423a5107ae915417105
AlbinBerisha <albin199915@gmail.com>
2018-11-06 00:21:18 +0100

    do sene

--- javascript20.js
+++ javascript20.js
@@ -1,30 +1,41 @@
 var image_index = 0;
 
 var slider_images = [
-    "Pictures\\prishtina.jpg",
-    "Pictures\\malisheva.jpg",
-    "Pictures\\prizreni.jpg"
+    "Pictures\\Galaxy Note9.jpg",
+    "Pictures\\Galaxy S6.jpg",
+    "Pictures\\prishtina.jpg"
+];
+
+var slider_overlays = [
+    "Galaxy S6",
+    "Galaxy Note9",
+    "Prishtina"
 ];
 
 function select(i) {
     var image = document.getElementById('slider-image');
-    var dots = document.getElementsByClassName('dots');
+    var overlay_name = document.getElementById('overlay-title');
     image_index = i;
     image.src = slider_images[image_index];
+    overlay_name.innerHTML = slider_overlays[image_index];
 }
 
 function next() {
     var image = document.getElementById('slider-image');
+    var overlay_name = document.getElementById('overlay-title');
     image_index++;
     if(image_index > 2)
         image_index = 0;
     image.src = slider_images[image_index];
+    overlay_name.innerHTML = slider_overlays[image_index];
 }
 
 function previous() {
     var image = document.getElementById('slider-image');
+    var overlay_name = document.getElementById('overlay-title');
     image_index--;
     if(image_index < 0)
         image_index = 2;
     image.src = slider_images[image_index];
+    overlay_name.innerHTML = slider_overlays[image_index];
 }
