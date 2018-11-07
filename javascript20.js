9cbdfe49fa6a30f1f1646af72a64527688526da8
AlbinBerisha <albin199915@gmail.com>
2018-11-08 00:46:09 +0100

    Effects

--- javascript20.js
+++ javascript20.js
@@ -2,14 +2,18 @@ var image_index = 0;
 
 var slider_images = [
     "Images\\Galaxy Note9.jpg",
-    "Images\\Galaxy S6.jpg",
-    "Images\\Gear VR.jpg"
+    "Images\\The Frame TV.jpg",
+    "Images\\Gear VR.jpg",
+    "Images\\Galaxy Watch.jpg",
+    "Images\\Galaxy A9.jpg"
 ];
 
 var slider_overlays = [
     "Galaxy Note9",
-    "Galaxy S6",
-    "Gear VR"
+    "The Frame TV",
+    "Gear VR",
+    "Galaxy Watch",
+    "Galaxy A9"
 ];
 
 function select(i) {
@@ -24,7 +28,7 @@ function next() {
     var image = document.getElementById('slider-image');
     var overlay_name = document.getElementById('overlay-title');
     image_index++;
-    if(image_index > 2)
+    if(image_index > 4)
         image_index = 0;
     image.src = slider_images[image_index];
     overlay_name.innerHTML = slider_overlays[image_index];
@@ -35,7 +39,9 @@ function previous() {
     var overlay_name = document.getElementById('overlay-title');
     image_index--;
     if(image_index < 0)
-        image_index = 2;
+        image_index = 4;
     image.src = slider_images[image_index];
     overlay_name.innerHTML = slider_overlays[image_index];
 }
+
+setInterval(next, 20000);
